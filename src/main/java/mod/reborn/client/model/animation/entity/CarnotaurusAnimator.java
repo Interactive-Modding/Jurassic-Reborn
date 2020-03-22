b33801159f226aae0c6cc9a74360b53ad9d4f486
Destruction <Dylan1268@icloud.com>
2020-03-22 13:41:01 -0400

    Update CarnotaurusAnimator.java

--- src/main/java/mod/reborn/client/model/animation/entity/CarnotaurusAnimator.java
+++ src/main/java/mod/reborn/client/model/animation/entity/CarnotaurusAnimator.java
@@ -4,7 +4,7 @@ import mod.reborn.client.model.AnimatableModel;
 import mod.reborn.client.model.animation.EntityAnimator;
 import mod.reborn.server.entity.dinosaur.CarnotaurusEntity;
 
-public class CarnotaurusAnimator extends EntityAnimator<CarnotaurusEntity>
+public class CarnotaurusAnimator extends EntityAnimator<CarnotaurusEntity> {
 
         protected void performAnimations(AnimatableModel parModel, CarnotaurusEntity entity, float limbSwing, float limbSwingAmount, float ticks, float rotationYaw, float rotationPitch, float scale) {
         }
