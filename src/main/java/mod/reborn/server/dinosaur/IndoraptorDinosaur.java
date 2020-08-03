5834ea27d74399aa11b057f9f6093c0e3ff71714
Destruction <Dylan1268@icloud.com>
2020-08-03 18:53:45 -0400

    Indoraptor items

--- src/main/java/mod/reborn/server/dinosaur/IndoraptorDinosaur.java
+++ src/main/java/mod/reborn/server/dinosaur/IndoraptorDinosaur.java
@@ -11,7 +11,7 @@ public class IndoraptorDinosaur extends Dinosaur implements Hybrid
 {
     private ResourceLocation texture;
 
-    public static final double SPEED = 0.4F;
+    public static final double SPEED = 0.45F;
 
     public IndoraptorDinosaur()
     {
@@ -21,8 +21,8 @@ public class IndoraptorDinosaur extends Dinosaur implements Hybrid
         this.setDinosaurType(DinosaurType.AGGRESSIVE);
         this.setDinosaurClass(IndoraptorEntity.class);
         this.setTimePeriod(TimePeriod.NONE); //TODO, it's a hybrid, what do you do here?
-        this.setEggColorMale(0xBEBABB, 0x95949A);
-        this.setEggColorFemale(0xBEBABB, 0x95949A);
+        this.setEggColorMale(0x787878, 0x2B2B2B);
+        this.setEggColorFemale(0x787878, 0x2B2B2B);
         this.setHealth(16, 70);
         this.setSpeed((SPEED -0.05), SPEED);
         this.setStrength(10, 40);
@@ -32,7 +32,7 @@ public class IndoraptorDinosaur extends Dinosaur implements Hybrid
         this.setSizeY(0.3F, 4F);
         this.setStorage(54);
         this.setDiet((Diet.CARNIVORE.get()));
-        this.setBones( "front_leg_bones", "hind_bones", "neck_vertebrae", "claw", "pelvis", "ribcage", "shoulder", "skull", "tail_vertebrae", "tooth");
+        this.setBones( "front_leg_bones", "hind_leg_bones", "foot_bones", "neck_vertebrae", "claw", "pelvis", "ribcage", "shoulder", "skull", "tail_vertebrae", "tooth");
         this.setHeadCubeName("Head");
         this.setScale(1.4F, 0.25F);
         this.setAttackBias(4000);
@@ -41,7 +41,8 @@ public class IndoraptorDinosaur extends Dinosaur implements Hybrid
         String[][] recipe = {
                 {"", "", "","neck_vertebrae","skull"},
                 {"tail_vertebrae", "pelvis", "ribcage","shoulder","tooth"},
-                {"", "hind_leg_bones", "", "front_leg_bones", "claw"}};
+                {"", "hind_leg_bones", "", "front_leg_bones", "claw"},
+                {"", "foot_bones"}};
         this.setRecipe(recipe);
 
         this.texture = new ResourceLocation(getDinosaurTexture("camouflage"));
