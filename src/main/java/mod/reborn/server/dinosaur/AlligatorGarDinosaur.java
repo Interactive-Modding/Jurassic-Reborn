da47b93f7c47bedf81718dee7a6af7f4afc67f72
Destruction <Dylan1268@icloud.com>
2020-08-17 01:30:59 -0400

    Tylosaurus

--- src/main/java/mod/reborn/server/dinosaur/AlligatorGarDinosaur.java
+++ src/main/java/mod/reborn/server/dinosaur/AlligatorGarDinosaur.java
@@ -24,7 +24,6 @@ public class AlligatorGarDinosaur extends Dinosaur
         this.setName("Alligator Gar");
         this.setDinosaurType(DinosaurType.PASSIVE);
         this.setDinosaurClass(AlligatorGarEntity.class);
-        this.setDinosaurType(DinosaurType.PASSIVE);
         this.setTimePeriod(TimePeriod.CRETACEOUS);
         this.setEggColorMale(0x707B94, 0x3B4963);
         this.setEggColorFemale(0x7C775E, 0x4D4A3B);
@@ -37,7 +36,6 @@ public class AlligatorGarDinosaur extends Dinosaur
         this.setEyeHeight(0.35F, 1.2F);
         this.setSizeX(0.1F, 1.1F);
         this.setSizeY(0.02F, .4F);
-        this.setStorage(9);
         this.setDiet(Diet.PISCIVORE.get().withModule(new Diet.DietModule(FoodType.FILTER)));
         this.setSleepTime(SleepTime.NO_SLEEP);
         this.setBones("anal_fin", "dorsal_fin", "pectoral_fin_bones", "pelvic_fin_bones", "scales", "skull", "spine", "tail_fin", "teeth");
@@ -52,7 +50,6 @@ public class AlligatorGarDinosaur extends Dinosaur
         this.setRandomFlock(false);
         this.setImprintable(false);
         this.setBirthType(BirthType.LIVE_BIRTH);
-        this.setStorage(4);
         String[][] recipe = {{"", "dorsal_fin", "", ""},
                 {"tail_fin", "spine", "pectoral_fin_bones", "skull"},
                 {"anal_fin", "", "pelvic_fin_bones", "teeth"}};
