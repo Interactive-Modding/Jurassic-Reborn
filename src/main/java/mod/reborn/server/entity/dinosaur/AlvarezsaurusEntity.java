6ed8faaffcea3f3c7b42dc2d9a1f4b4aef479309
Destruction <Dylan1268@icloud.com>
2020-08-19 22:30:14 -0400

    Fix

--- src/main/java/mod/reborn/server/entity/dinosaur/AlvarezsaurusEntity.java
+++ src/main/java/mod/reborn/server/entity/dinosaur/AlvarezsaurusEntity.java
@@ -1,12 +1,15 @@
 package mod.reborn.server.entity.dinosaur;
 
 import mod.reborn.server.entity.DinosaurEntity;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityAnimal;
+import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.world.World;
 
 public class AlvarezsaurusEntity extends DinosaurEntity {
 
     public AlvarezsaurusEntity(World world) {
         super(world);
-        this.target(DodoEntity.class, CompsognathusEntity.class, HypsilophodonEntity.class, LeptictidiumEntity.class, MicroraptorEntity.class, MicroceratusEntity.class, DimorphodonEntity.class, LeaellynasauraEntity.class, ProtoceratopsEntity.class, OviraptorEntity.class, MussaurusEntity.class, OthnieliaEntity.class, TroodonEntity.class, SegisaurusEntity.class);
+        this.target(DodoEntity.class, CompsognathusEntity.class, HypsilophodonEntity.class, EntityAnimal.class, EntityMob.class, LeptictidiumEntity.class, MicroraptorEntity.class, MicroceratusEntity.class, DimorphodonEntity.class, LeaellynasauraEntity.class, ProtoceratopsEntity.class, OviraptorEntity.class, MussaurusEntity.class, OthnieliaEntity.class, TroodonEntity.class, SegisaurusEntity.class);
     }
 }
