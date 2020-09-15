dba61716d5854469183b48cb07cc4c61a45e3179
Destruction <Dylan1268@icloud.com>
2020-09-15 09:56:19 -0400

    indominus fix

--- src/main/java/mod/reborn/server/entity/dinosaur/IndominusEntity.java
+++ src/main/java/mod/reborn/server/entity/dinosaur/IndominusEntity.java
@@ -155,7 +155,6 @@ public class IndominusEntity extends DinosaurEntity
             case INJURED:
                 return SoundHandler.INDOMINUS_HURT;
             case ROARING:
-            case CALLING:
                 return SoundHandler.INDOMINUS_ROAR;
         }
 
