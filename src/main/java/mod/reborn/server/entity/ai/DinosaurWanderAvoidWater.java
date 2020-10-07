1d80463f88e32dcce74b216436b3de5b90ec7361
Collinvht <collin060204@gmail.com>
2020-10-07 12:10:48 +0200

    Hotfix crash

--- src/main/java/mod/reborn/server/entity/ai/DinosaurWanderAvoidWater.java
+++ src/main/java/mod/reborn/server/entity/ai/DinosaurWanderAvoidWater.java
@@ -34,8 +34,9 @@ public class DinosaurWanderAvoidWater extends DinosaurWanderEntityAI {
 	Vec3d vec3d = null;
 	for(int i = 0; i < 100; i++) {
 	    Vec3d vec = RandomPositionGenerator.getLandPos(this.entity, walkradius + 5, walkradius);
-	    assert vec != null;
-        if(vec3d == null || this.entity.getPositionVector().distanceTo(vec) < this.entity.getPositionVector().distanceTo(vec3d)) {
+	    if(vec == null) {
+		continue;
+	    } else if(vec3d == null || this.entity.getPositionVector().distanceTo(vec) < this.entity.getPositionVector().distanceTo(vec3d)) {
 		vec3d = vec;
 	    }
 	}
