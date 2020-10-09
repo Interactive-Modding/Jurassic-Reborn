53b23c84ef1d657fcea0c37792ff84e5625bf2f7
Destruction <Dylan1268@icloud.com>
2020-10-09 17:15:16 -0400

    Compsognathus remodel

--- src/main/java/mod/reborn/client/model/animation/entity/CompsognathusAnimator.java
+++ src/main/java/mod/reborn/client/model/animation/entity/CompsognathusAnimator.java
@@ -7,80 +7,8 @@ import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import mod.reborn.server.entity.dinosaur.CompsognathusEntity;
 
-@SideOnly(Side.CLIENT)
-public class CompsognathusAnimator extends EntityAnimator<CompsognathusEntity>
-{
-    @Override
-    protected void performAnimations(AnimatableModel model, CompsognathusEntity entity, float f, float f1, float ticks, float rotationYaw, float rotationPitch, float scale)
-    {
-        AdvancedModelRenderer abdomen = model.getCube("abdomen");
-        AdvancedModelRenderer upperBody = model.getCube("Upper body");
+public class CompsognathusAnimator extends EntityAnimator<CompsognathusEntity> {
 
-        AdvancedModelRenderer head = model.getCube("Head");
-
-        AdvancedModelRenderer neck1 = model.getCube("Neck 1");
-        AdvancedModelRenderer neck2 = model.getCube("Neck 2");
-        AdvancedModelRenderer neck3 = model.getCube("Neck 3");
-        AdvancedModelRenderer neck4 = model.getCube("Neck 4");
-        AdvancedModelRenderer neck5 = model.getCube("Neck 5");
-        AdvancedModelRenderer neck6 = model.getCube("Neck 6");
-        AdvancedModelRenderer neck7 = model.getCube("Neck 7");
-
-        AdvancedModelRenderer lowerJaw = model.getCube("Lower Jaw");
-
-        AdvancedModelRenderer leftThigh = model.getCube("Left thigh");
-        AdvancedModelRenderer leftMidLeg = model.getCube("Left mid leg");
-        AdvancedModelRenderer leftShin = model.getCube("Left shin");
-        AdvancedModelRenderer leftFoot = model.getCube("Left foot");
-
-        AdvancedModelRenderer rightThigh = model.getCube("Right thigh");
-        AdvancedModelRenderer rightMidLeg = model.getCube("Right mid leg");
-        AdvancedModelRenderer rightShin = model.getCube("Right shin");
-        AdvancedModelRenderer rightFoot = model.getCube("Right foot");
-
-        AdvancedModelRenderer tail1 = model.getCube("Tail 1");
-        AdvancedModelRenderer tail2 = model.getCube("Tail 2");
-        AdvancedModelRenderer tail3 = model.getCube("Tail 3");
-        AdvancedModelRenderer tail4 = model.getCube("Tail 4");
-        AdvancedModelRenderer tail5 = model.getCube("Tail 5");
-
-        AdvancedModelRenderer leftArm = model.getCube("Left arm");
-        AdvancedModelRenderer leftForeArm = model.getCube("Left forearm");
-        AdvancedModelRenderer leftHand = model.getCube("Left hand");
-
-        AdvancedModelRenderer rightArm = model.getCube("Right arm");
-        AdvancedModelRenderer rightForeArm = model.getCube("Right forearm");
-        AdvancedModelRenderer rightHand = model.getCube("Right hand");
-
-        AdvancedModelRenderer[] tail = new AdvancedModelRenderer[] { tail5, tail4, tail3, tail2, tail1 };
-        AdvancedModelRenderer[] neck = new AdvancedModelRenderer[] { head, neck7, neck6, neck5, neck4, neck3, neck2, neck1, upperBody };
-
-        // f = ticks;
-        // f1 = 0.4F;
-
-        float globalSpeed = 1.8F;
-        float globalDegree = 1.5F;
-        float globalHeight = 0.45F;
-
-        model.bob(abdomen, globalSpeed * 0.5F, globalHeight * 1.0F, false, f, f1);
-        model.bob(leftThigh, globalSpeed * 0.5F, globalHeight * 1.0F, false, f, f1);
-        model.bob(rightThigh, globalSpeed * 0.5F, globalHeight * 1.0F, false, f, f1);
-        model.chainWave(tail, globalSpeed * 0.5F, globalHeight * 0.125F, 2, f, f1);
-        model.chainWave(neck, globalSpeed * 0.5F, globalHeight * 0.0625F, -2, f, f1);
-
-        model.walk(leftThigh, 0.5F * globalSpeed, globalDegree * 0.7F, false, 3.14F, 0.2F, f, f1);
-        model.walk(leftMidLeg, 0.5F * globalSpeed, globalDegree * 0.6F, false, 1.5F, 0.3F, f, f1);
-        model.walk(leftShin, 0.5F * globalSpeed, globalDegree * 0.8F, false, -1F, -0.1F, f, f1);
-        model.walk(leftFoot, 0.5F * globalSpeed, globalDegree * 1.5F, true, -1F, 1F, f, f1);
-
-        model.walk(rightThigh, 0.5F * globalSpeed, globalDegree * 0.7F, true, 3.14F, 0.2F, f, f1);
-        model.walk(rightMidLeg, 0.5F * globalSpeed, globalDegree * 0.6F, true, 1.5F, 0.3F, f, f1);
-        model.walk(rightShin, 0.5F * globalSpeed, globalDegree * 0.8F, true, -1F, -0.1F, f, f1);
-        model.walk(rightFoot, 0.5F * globalSpeed, globalDegree * 1.5F, false, -1F, 1F, f, f1);
-
-        model.chainWave(tail, 0.125F * globalSpeed, globalHeight * 0.125F, 2, ticks, 0.0625F);
-        model.chainWave(neck, 0.125F * globalSpeed, globalHeight * 0.125F, -2, ticks, 0.0625F);
-
-        entity.tailBuffer.applyChainSwingBuffer(tail);
+    protected void performAnimations(AnimatableModel parModel, CompsognathusEntity entity, float limbSwing, float limbSwingAmount, float ticks, float rotationYaw, float rotationPitch, float scale) {
     }
 }
--- src/main/java/mod/reborn/server/entity/dinosaur/CompsognathusEntity.java
+++ src/main/java/mod/reborn/server/entity/dinosaur/CompsognathusEntity.java
@@ -1,10 +1,12 @@
 package mod.reborn.server.entity.dinosaur;
 
+import mod.reborn.RebornMod;
 import mod.reborn.client.model.animation.EntityAnimation;
 import mod.reborn.server.entity.DinosaurEntity;
 import mod.reborn.server.entity.ai.LeapingMeleeEntityAI;
 import mod.reborn.server.entity.ai.RaptorLeapEntityAI;
 import mod.reborn.server.entity.animal.GoatEntity;
+import net.minecraft.client.resources.Locale;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.ai.EntityAIBase;
 import net.minecraft.entity.ai.EntityAIHurtByTarget;
@@ -12,11 +14,20 @@ import net.minecraft.entity.monster.EntityMob;
 import net.minecraft.entity.passive.EntityAnimal;
 import net.minecraft.entity.passive.EntityVillager;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.network.datasync.DataParameter;
+import net.minecraft.network.datasync.DataSerializers;
+import net.minecraft.network.datasync.EntityDataManager;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+import scala.tools.nsc.doc.model.Class;
 
 public class CompsognathusEntity extends DinosaurEntity {
+    private static final DataParameter<Integer> VARIANT = EntityDataManager.createKey(CompsognathusEntity.class, DataSerializers.VARINT);
+
     public CompsognathusEntity(World world) {
         super(world);
+        this.setVariant(this.getRNG().nextInt(6));
         this.doesEatEggs(true);
         this.target(DodoEntity.class, OthnieliaEntity.class, MicroceratusEntity.class, MicroraptorEntity.class, CrassigyrinusEntity.class, LeptictidiumEntity.class, EntityPlayer.class, EntityAnimal.class, EntityVillager.class, EntityMob.class, GoatEntity.class);
         this.tasks.addTask(0, new LeapingMeleeEntityAI(this, this.dinosaur.getAttackSpeed()));
@@ -43,6 +54,7 @@ public class CompsognathusEntity extends DinosaurEntity {
             super(CompsognathusEntity.this, false, new Class[0]);
         }
 
+
         public void startExecuting() {
             if (CompsognathusEntity.this.herd.size() >= 3) {
                 super.startExecuting();
@@ -60,6 +72,45 @@ public class CompsognathusEntity extends DinosaurEntity {
 
 
         }
+    }
+    public void entityInit() {
+        super.entityInit();
+        this.dataManager.register(VARIANT, 0);
+    }
+
+    public void writeEntityToNBT(NBTTagCompound tagCompound) {
+        super.writeEntityToNBT(tagCompound);
+        tagCompound.setInteger("Variant", this.getVariant());
+    }
+
+    public void readEntityFromNBT(NBTTagCompound tagCompound) {
+        super.readEntityFromNBT(tagCompound);
+        this.setVariant(tagCompound.getInteger("Variant"));
+    }
+
+    public void setVariant(int value){
+        this.dataManager.set(VARIANT, value);
+    }
+
+    public int getVariant(){
+        return this.dataManager.get(VARIANT);
+    }
+
+    public ResourceLocation getTexture(){
+        switch(getVariant()){
+            case 0: default: return texture("nostripes");
+            case 1: return texture("stripes");
+            case 2: return texture("lightstripes");
+            case 3: return texture("orange");
+            case 4: return texture("blue");
+            case 5: return texture("purple");
+        }
+    }
+    private ResourceLocation texture(String variant){
+        String formattedName = this.dinosaur.getName().toLowerCase(Locale.ENGLISH).replaceAll(" ", "_");
+        String baseTextures = "textures/entities/" + formattedName + "/";
+        String texture = baseTextures + formattedName;
+        return isMale()?new ResourceLocation(RebornMod.MODID, texture + "_male_" + variant + ".png"):new ResourceLocation(RebornMod.MODID, texture + "_female_" + variant +".png");
     }
 }
 
