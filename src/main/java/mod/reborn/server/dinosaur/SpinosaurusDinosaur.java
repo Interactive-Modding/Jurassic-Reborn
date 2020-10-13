3da710ba910d5a27409eb9bddc6641efb4d6f831
Destruction <Dylan1268@icloud.com>
2020-10-13 14:32:16 -0400

    Spinosaurus remodel ~~missing skeleton ~~missing death pose

--- src/main/java/mod/reborn/server/dinosaur/SpinosaurusDinosaur.java
+++ src/main/java/mod/reborn/server/dinosaur/SpinosaurusDinosaur.java
@@ -23,13 +23,13 @@ public class SpinosaurusDinosaur extends Dinosaur
         this.setStrength(10, 20);
         this.setMaximumAge(fromDays(55));
         this.setEyeHeight(0.6F, 3.8F);
-        this.setSizeX(0.6F, 3.0F);
-        this.setSizeY(0.8F, 4.8F);
+        this.setSizeX(0.5F, 2.9F);
+        this.setSizeY(0.7F, 4.7F);
         this.setStorage(54);
         this.setDiet((Diet.PCARNIVORE.get()));
         this.setBones("skull", "tooth", "arm_bones", "claw", "foot_bones", "leg_bones", "neck_vertebrae", "ribcage", "shoulder", "tail_vertebrae", "pelvis");
         this.setHeadCubeName("Head");
-        this.setScale(2.37F, 0.3F);
+        this.setScale(1.87F, 0.3F);
         this.setAttackBias(900);
         this.shouldDefendOffspring();
         this.setBreeding(false, 2, 6, 40, false, true);
