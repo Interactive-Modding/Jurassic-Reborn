ebc64b75950461676bde116afa5dfec079a85a19
Destruction <Dylan1268@icloud.com>
2020-11-04 18:38:04 -0500

    Tyrannosaurus remodel completed

--- src/main/java/mod/reborn/server/entity/dinosaur/TyrannosaurusEntity.java
+++ src/main/java/mod/reborn/server/entity/dinosaur/TyrannosaurusEntity.java
@@ -16,20 +16,12 @@ import net.minecraft.util.SoundEvent;
 import net.minecraft.world.World;
 
 public class TyrannosaurusEntity extends DinosaurEntity {
-    private int stepCount = 0;
-
-    public LegSolverBiped legSolver;
 
     public TyrannosaurusEntity(World world) {
         super(world);
         this.target(AchillobatorEntity.class, SinoceratopsEntity.class, AlligatorGarEntity.class, AllosaurusEntity.class, AlvarezsaurusEntity.class, AnkylosaurusEntity.class, ApatosaurusEntity.class, BaryonyxEntity.class, BeelzebufoEntity.class, VelociraptorBlueEntity.class, CarnotaurusEntity.class, CearadactylusEntity.class, CeratosaurusEntity.class, VelociraptorCharlieEntity.class, ChasmosaurusEntity.class, ChilesaurusEntity.class, CoelurusEntity.class, CompsognathusEntity.class, CorythosaurusEntity.class, CrassigyrinusEntity.class, VelociraptorDeltaEntity.class, DilophosaurusEntity.class, DimorphodonEntity.class, DiplocaulusEntity.class, DodoEntity.class, VelociraptorEchoEntity.class, EdmontosaurusEntity.class, GallimimusEntity.class, GiganotosaurusEntity.class, GuanlongEntity.class, HerrerasaurusEntity.class, HyaenodonEntity.class, HypsilophodonEntity.class, IndominusEntity.class, LambeosaurusEntity.class, LeaellynasauraEntity.class, LeptictidiumEntity.class, LudodactylusEntity.class, MajungasaurusEntity.class, MamenchisaurusEntity.class, MammothEntity.class, MegapiranhaEntity.class, MetriacanthosaurusEntity.class, MicroceratusEntity.class, MicroraptorEntity.class, MoganopterusEntity.class, MussaurusEntity.class, OrnithomimusEntity.class, OthnieliaEntity.class, OviraptorEntity.class, PachycephalosaurusEntity.class, ParasaurolophusEntity.class, PostosuchusEntity.class, ProceratosaurusEntity.class, ProtoceratopsEntity.class, PteranodonEntity.class, QuetzalEntity.class, RugopsEntity.class, SegisaurusEntity.class, SpinosaurusEntity.class, StegosaurusEntity.class, StyracosaurusEntity.class, SuchomimusEntity.class, TherizinosaurusEntity.class, TriceratopsEntity.class, TroodonEntity.class, TropeognathusEntity.class , VelociraptorEntity.class, ZhenyuanopterusEntity.class, GoatEntity.class, EntityPlayer.class, EntityAnimal.class, EntityVillager.class);
         this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));
 
-    }
-
-    @Override
-    protected LegSolverBiped createLegSolver() {
-        return this.legSolver = new LegSolverBiped(-0.5F, 1.0F, 1.0F);
     }
 
     @Override
@@ -61,16 +53,5 @@ public class TyrannosaurusEntity extends DinosaurEntity {
     public SoundEvent getBreathingSound() {
         return SoundHandler.TYRANNOSAURUS_BREATHING;
     }
-   
-    @Override
-    public void onUpdate() {
-        super.onUpdate();
-        if (this.onGround && !this.isInWater()) {
-            if (this.moveForward > 0 && (this.posX - this.prevPosX > 0 || this.posZ - this.prevPosZ > 0) && this.stepCount <= 0) {
-                this.playSound(SoundHandler.STOMP, (float) this.interpolate(0.1F, 1.0F), this.getSoundPitch());
-                this.stepCount = 65;
-            }
-            this.stepCount -= this.moveForward * 9.5;
-        }
-    }
+
 }
