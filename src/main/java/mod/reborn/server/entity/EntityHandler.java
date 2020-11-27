6ab23976ce8d28e7fd8873a3c11c999cc192e028
Destruction <Dylan1268@icloud.com>
2020-11-26 21:20:35 -0500

    Disabled the Shark spawn for 1.0

--- src/main/java/mod/reborn/server/entity/EntityHandler.java
+++ src/main/java/mod/reborn/server/entity/EntityHandler.java
@@ -233,7 +233,7 @@ public class EntityHandler {
         ArrayList<Biome> biomeBeach = new ArrayList<>(BiomeDictionary.getBiomes(BiomeDictionary.Type.BEACH));
 
         EntityRegistry.addSpawn(EntityCrab.class, 20, 4, 8, EnumCreatureType.CREATURE, biomeBeach.toArray(new Biome[biomeBeach.size()]));
-        EntityRegistry.addSpawn(EntityShark.class, 3, 1, 4, EnumCreatureType.WATER_CREATURE, Biomes.OCEAN);
+    //EntityRegistry.addSpawn(EntityShark.class, 3, 1, 4, EnumCreatureType.WATER_CREATURE, Biomes.OCEAN);
         EntitySpawnPlacementRegistry.setPlacementType(EntityShark.class, EntityLiving.SpawnPlacementType.IN_WATER);
 
         registerEntity(DinosaurEggEntity.class, "Dinosaur Egg");
