d6eff85fa4a398ac7bd07b81755101f00cf69e86
thatdude <102991577+thatdudew@users.noreply.github.com>
2023-11-11 21:06:25 +0000

    Cleaned up TabulaModelHelper

--- src/main/java/mod/reborn/server/tabula/TabulaModelHelper.java
+++ src/main/java/mod/reborn/server/tabula/TabulaModelHelper.java
@@ -16,71 +16,43 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 public class TabulaModelHelper {
+
     public static TabulaCubeContainer getCubeByName(String name, TabulaModelContainer model) {
-        List<TabulaCubeContainer> allCubes = getAllCubes(model);
-
-        for (TabulaCubeContainer cube : allCubes) {
-            if (cube.getName().equals(name)) {
-                return cube;
-            }
-        }
-
-        return null;
+        return getAllCubes(model).stream()
+                .filter(cube -> cube.getName().equals(name))
+                .findFirst()
+                .orElse(null);
     }
 
     public static TabulaCubeContainer getCubeByIdentifier(String identifier, TabulaModelContainer model) {
-        List<TabulaCubeContainer> allCubes = getAllCubes(model);
-
-        for (TabulaCubeContainer cube : allCubes) {
-            if (cube.getIdentifier().equals(identifier)) {
-                return cube;
-            }
-        }
-
-        return null;
+        return getAllCubes(model).stream()
+                .filter(cube -> cube.getIdentifier().equals(identifier))
+                .findFirst()
+                .orElse(null);
     }
 
     public static List<TabulaCubeContainer> getAllCubes(TabulaModelContainer model) {
         List<TabulaCubeContainer> cubes = new ArrayList<>();
-
         try {
-            for (TabulaCubeGroupContainer cubeGroup : model.getCubeGroups()) {
-                cubes.addAll(traverse(cubeGroup));
-            }
-
-            for (TabulaCubeContainer cube : model.getCubes()) {
-                cubes.addAll(traverse(cube));
-            }
+            model.getCubeGroups().forEach(cubeGroup -> cubes.addAll(traverse(cubeGroup)));
+            model.getCubes().forEach(cube -> cubes.addAll(traverse(cube)));
         } catch (Exception e) {
             RebornMod.getLogger().error("Failed to load all cubes for model {} by {}", model.getName(), model.getAuthor(), e);
         }
-
         return cubes;
     }
 
     private static List<TabulaCubeContainer> traverse(TabulaCubeGroupContainer group) {
         List<TabulaCubeContainer> retCubes = new ArrayList<>();
-
-        for (TabulaCubeContainer child : group.getCubes()) {
-            retCubes.addAll(traverse(child));
-        }
-
-        for (TabulaCubeGroupContainer child : group.getCubeGroups()) {
-            retCubes.addAll(traverse(child));
-        }
-
+        group.getCubes().forEach(child -> retCubes.addAll(traverse(child)));
+        group.getCubeGroups().forEach(child -> retCubes.addAll(traverse(child)));
         return retCubes;
     }
 
     private static List<TabulaCubeContainer> traverse(TabulaCubeContainer cube) {
         List<TabulaCubeContainer> retCubes = new ArrayList<>();
-
         retCubes.add(cube);
-
-        for (TabulaCubeContainer child : cube.getChildren()) {
-            retCubes.addAll(traverse(child));
-        }
-
+        cube.getChildren().forEach(child -> retCubes.addAll(traverse(child)));
         return retCubes;
     }
 
@@ -88,13 +60,11 @@ public class TabulaModelHelper {
         if (!path.startsWith("/")) {
             path = "/" + path;
         }
-
         if (!path.endsWith(".tbl")) {
             path += ".tbl";
         }
-
         InputStream stream = TabulaModelHelper.class.getResourceAsStream(path);
-        return TabulaModelHelper.loadTabulaModel(getModelJsonStream(path, stream));
+        return loadTabulaModel(getModelJsonStream(path, stream));
     }
 
     public static TabulaModelContainer loadTabulaModel(InputStream stream) {
@@ -104,24 +74,21 @@ public class TabulaModelHelper {
     private static InputStream getModelJsonStream(String name, InputStream file) throws IOException {
         ZipInputStream zip = new ZipInputStream(file);
         ZipEntry entry;
-
         while ((entry = zip.getNextEntry()) != null) {
             if (entry.getName().equals("model.json")) {
                 return zip;
             }
         }
-
         throw new RuntimeException("No model.json present in " + name);
     }
 
     public static TabulaModelContainer loadTabulaModel(ResourceLocation location) throws IOException {
-
         String path = "/assets/" + location.getResourceDomain() + "/" + location.getResourcePath() + ".tbl";
         try (InputStream stream = TabulaModelHelper.class.getResourceAsStream(path)) {
             if (stream == null) {
                 throw new IllegalArgumentException("Could not find tabula model at " + location);
             }
-            return TabulaModelHelper.loadTabulaModel(getModelJsonStream(path, stream));
+            return loadTabulaModel(getModelJsonStream(path, stream));
         }
     }
 }
