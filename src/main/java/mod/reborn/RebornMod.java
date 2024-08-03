5d10387c7a93837f211e06c0e3cc061d40b3e338
Vitiate <40134493+Darth-Vitiate@users.noreply.github.com>
2024-08-03 00:32:58 -0700

    Mod Rename

--- src/main/java/mod/reborn/RebornMod.java
+++ src/main/java/mod/reborn/RebornMod.java
@@ -18,7 +18,7 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = RebornMod.MODID, name = RebornMod.NAME, version = RebornMod.VERSION, dependencies = "required-after:llibrary@[1.7.15,);required-after:forge@[14.23.5.2772,)")
 public class RebornMod {
     public static final String MODID = "rebornmod";
-    public static final String NAME = "JWReborn";
+    public static final String NAME = "Jurassic Reborn";
     public static final String VERSION = "1.3.32";
 
     private final boolean TestEnv = false;
