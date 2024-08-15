ce62a508032f25d3991c1095920b58761c15c634
Vitiate <40134493+Darth-Vitiate@users.noreply.github.com>
2024-08-15 16:16:07 -0400

    fix cultivator crash ?

--- src/main/java/mod/reborn/client/render/block/CultivatorRenderer.java
+++ src/main/java/mod/reborn/client/render/block/CultivatorRenderer.java
@@ -1,15 +1,13 @@
 package mod.reborn.client.render.block;
 
-import mod.reborn.client.model.animation.EntityAnimation;
+import mod.reborn.server.block.entity.CultivatorBlockEntity;
 import mod.reborn.server.entity.DinosaurEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.renderer.block.model.ItemCameraTransforms;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
-import net.minecraft.item.ItemStack;
-import javax.vecmath.Vector3f;
-import mod.reborn.server.block.entity.CultivatorBlockEntity;
 import org.lwjgl.opengl.GL11;
+
+import javax.vecmath.Vector3f;
 
 public class CultivatorRenderer extends TileEntitySpecialRenderer<CultivatorBlockEntity> {
     private Minecraft mc = Minecraft.getMinecraft();
@@ -21,28 +19,18 @@ public class CultivatorRenderer extends TileEntitySpecialRenderer<CultivatorBloc
         GlStateManager.translate(x + 0.5, y + 1, z + 0.5);
 
         if(tileEntity.isProcessing(0) && tileEntity.getField(1) != 0) {
-            long timer = getWorld().getTotalWorldTime();
+            long timer = getWorld() != null ? getWorld().getTotalWorldTime() : 0;
             DinosaurEntity entity = tileEntity.getDinosaurEntity();
-            float percentageDone = tileEntity.getField(0) / (float)tileEntity.getField(1);
-            Vector3f vec = ((DinosaurEntity) entity).getDinosaurCultivatorRotation();
-            GlStateManager.rotate(90F, -1, 0, 0);
-            GlStateManager.rotate(vec.y, 0, 0, 0);
-            GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
-            GlStateManager.rotate(vec.z, 0, 0, 0);
-            GlStateManager.scale(percentageDone, percentageDone, percentageDone);
-            GlStateManager.enableLight(2);
-            GlStateManager.scale(.04,.04,.04);
             if (entity != null) {
-
-
-
-
-
-
-
-
-
-
+                float percentageDone = tileEntity.getField(0) / (float)tileEntity.getField(1);
+                Vector3f vec = entity.getDinosaurCultivatorRotation();
+                GlStateManager.rotate(90F, -1, 0, 0);
+                GlStateManager.rotate(vec.y, 0, 0, 0);
+                GL11.glRotatef(180.0F, 0.0F, 0.0F, 1.0F);
+                GlStateManager.rotate(vec.z, 0, 0, 0);
+                GlStateManager.scale(percentageDone, percentageDone, percentageDone);
+                GlStateManager.enableLight(2);
+                GlStateManager.scale(.04, .04, .04);
 
                 this.mc.getRenderManager().renderEntity(entity, 0, 0, 0, 0, 0, false);
             }
